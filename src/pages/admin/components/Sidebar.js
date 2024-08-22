eb2df7ecfccb7294ab9ae9f415178806e5a759b7
Darshan Unagar <160447032+Darshan-unagar@users.noreply.github.com>
2024-08-22 12:05:30 +0530

    Update Sidebar.js

--- src/pages/admin/components/Sidebar.js
+++ src/pages/admin/components/Sidebar.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { MenuOutlined, CloseOutlined, HomeOutlined, LogoutOutlined,PlusOutlined,VideoCameraOutlined   } from '@ant-design/icons';
+import { MenuOutlined, CloseOutlined, HomeOutlined, LogoutOutlined, PlusOutlined, VideoCameraOutlined, UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom'; // Import Link for navigation
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
- 
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
@@ -35,7 +34,7 @@ const Sidebar = () => {
           <ul className="space-y-2 font-medium">
             <li>
               <Link
-                to="/admin/home" // Replace with the correct path for All Movies Page
+                to="/admin/home" // Replace with the correct path for Home Page
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <HomeOutlined className="w-5 h-5" />
@@ -47,17 +46,26 @@ const Sidebar = () => {
                 to="/admin/movies" // Replace with the correct path for All Movies Page
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <VideoCameraOutlined  className="w-5 h-5" />
+                <VideoCameraOutlined className="w-5 h-5" />
                 <span className="ms-3">All Movies</span>
               </Link>
             </li>
             <li>
               <Link
-                to="/admin/movies/create" // Replace with the correct path for All Movies Page
+                to="/admin/movies/create" // Replace with the correct path for Add New Movie Page
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <PlusOutlined  className="w-5 h-5" />
+                <PlusOutlined className="w-5 h-5" />
                 <span className="ms-3">Add New Movie</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/admin/users" // Path for Manage Users Page
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <UserOutlined className="w-5 h-5" />
+                <span className="ms-3">Manage Users</span>
               </Link>
             </li>
             <li className="flex items-center p-2 cursor-pointer" onClick={handleLogout}>
