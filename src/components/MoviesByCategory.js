3fc12c86524f93f4440a15f18dafd7f568693d1e
Darshan Unagar <160447032+Darshan-unagar@users.noreply.github.com>
2024-09-05 14:29:40 +0530

    Update MoviesByCategory.js

--- src/components/MoviesByCategory.js
+++ src/components/MoviesByCategory.js
@@ -17,7 +17,7 @@ const MoviesByCategory = () => {
     refreshPage();
     const fetchMovies = async () => {
       try {
-        const response = await axios.get(`https://netstar.info.codesquareinfotech.com/movies/category/${category}`);
+        const response = await axios.get(`https://movie-app-backend-bthx.onrender.com/movies/category/${category}`);
         setMovies(response.data);
       } catch (error) {
         console.error('Error fetching movies:', error);
